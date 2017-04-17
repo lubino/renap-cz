b0da48381733975bd253897375e62104e8ea89c9
Lubos Strapko <lubino@lubino.sk>
2017-04-17 22:26:28 +0200

    Trying to use Mustache

--- Package.swift
+++ Package.swift
@@ -3,14 +3,18 @@ import PackageDescription
 let package = Package(
     name: "renap-cz",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        //.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
+        //.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 8),
+        //.Package(url: "https://github.com/groue/GRMustache.swift", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 5),
     ],
     exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
-        ]
+        //"Config",
+        //"Database",
+        //"Localization",
+        //"Public",
+        //"Resources",
+    ]
 )
 
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,31 @@
 import Vapor
+//import VaporPostgreSQL
+import Mustache
 
-let drop = Droplet()
+let drop = Droplet(
+//    providers: [VaporPostgreSQL.Provider.self]
+)
+
+drop.get("version") { request in
+//  if let db = drop.database?.driver as? PostgreSQLDriver {
+//   let version = try db.raw("SELECT version()")
+//    return try JSON(node: version)
+//  } else {
+    return "No db connection"
+//  }
+}
 
 drop.get("hello") { req in
     return "Hello, world."
 }
 
-drop.get("") { req in
-    return "Hello, world"
+drop.get { req in
+    let data: [String: Any] = [
+        "name": "Arthur",
+        "late": true
+    ]
+    let template = try Template(string: "Hello {{name}}")
+    return template.render(data)
 }
 
 drop.run()
