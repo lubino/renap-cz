0b605981ee3db0481b6b498bdfeeaf2c226cc90f
Lubos Strapko <lubos.strapko@apollogames.com>
2017-04-27 12:41:27 +0200

    Testing DB connection

--- Package.swift
+++ Package.swift
@@ -4,10 +4,11 @@ let package = Package(
     name: "renap-cz",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        //.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
-        //.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
+        //.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 7),
         //.Package(url: "https://github.com/groue/GRMustache.swift", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 5),
+        //.Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 5),
+        //.Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0),
     ],
     exclude: [
         //"Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,31 +1,53 @@
 import Vapor
-//import VaporPostgreSQL
-import Mustache
+import VaporPostgreSQL
+import PostgreSQL
+//import Mustache
+//import VaporMustache
 
 let drop = Droplet(
 //    providers: [VaporPostgreSQL.Provider.self]
 )
 
+try drop.addProvider(VaporPostgreSQL.Provider)
+
 drop.get("version") { request in
-//  if let db = drop.database?.driver as? PostgreSQLDriver {
-//   let version = try db.raw("SELECT version()")
-//    return try JSON(node: version)
-//  } else {
+  if let db = drop.database?.driver as? PostgreSQLDriver {
+   let version = try db.raw("SELECT version()")
+    return try JSON(node: version)
+  } else {
     return "No db connection"
-//  }
+  }
 }
 
 drop.get("hello") { req in
     return "Hello, world."
 }
 
-drop.get { req in
+drop.get("t") { req in
     let data: [String: Any] = [
         "name": "Arthur",
         "late": true
     ]
-    let template = try Template(string: "Hello {{name}}")
-    return template.render(data)
+    //let template = try Template(string: "Hello {{name}}")
+    //return template.render(data)
+    let postgreSQL =  PostgreSQL.Database(
+        host: "ec2-54-247-189-141.eu-west-1.compute.amazonaws.com",
+        dbname: "d45b0fv9ofti1j",
+        user: "samyfioimiutbn",
+        password: "d6f9b26890479397b198fe278dbd34bc2dca6f05c105dd07caee7fd40a0bb4fb"
+    )
+    
+    let results = try postgreSQL.execute("SELECT * FROM web OrderBy name", [])
+    var result = ""
+    for row in results {
+        for column in row {
+            let key = column.key
+            let value = column.value.string
+            result = "\(result), \(key):\(value!)"
+        }
+    }
+    
+    return result;
 }
 
 drop.run()
